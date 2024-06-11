a9758dfd5048abdaa3cb767372c40bbefc799bc7
Gabriel Rossin Ferrante <91850393+gabrielFerrante1@users.noreply.github.com>
2024-06-11 01:35:06 -0300

    Feat: add animate btn

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,20 +1,30 @@
 import { AreaChart, RotateCcw } from "lucide-react"
 import { useGameStore } from "../../stores/gameStore"
+import { motion, AnimatePresence } from "framer-motion"
 
 export const Footer = () => {
     const { status } = useGameStore()
 
     return (
         <div className="flex items-center gap-4">
-            <button className="btn-secondary flex-1">
+            <button className="btn-secondary flex-1" >
                 <AreaChart className="size-5" />
                 Ranking
             </button>
 
-            <button className="btn-primary flex-1">
-                <RotateCcw className="size-5" strokeWidth={2.5} />
-                Recomeçar
-            </button>
-        </div>
+            <AnimatePresence>
+                {status === "playing" &&
+                    <motion.button
+                        initial={{ opacity: 0, y: 30 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, y: 30 }}
+                        className="btn-primary duration-0 flex-1"
+                    >
+                        <RotateCcw className="size-5" strokeWidth={2.5} />
+                        Reiniciar
+                    </motion.button>
+                }
+            </AnimatePresence>
+        </div >
     )
 }
