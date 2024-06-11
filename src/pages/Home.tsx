d5a310dee939d476912f77d29e4a6402d31b653e
Gabriel Rossin Ferrante <91850393+gabrielFerrante1@users.noreply.github.com>
2024-06-11 00:27:47 -0300

    Feat: add home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,19 @@ export const Home = () => {
     return (
         <main>
             <Card>
-                <h1>Home</h1>
+                <div className="flex flex-col items-center gap-12 py-8">
+                    <div className="text-center">
+                        <p className="text-slate-300 text-lg font-bold">
+                            Seja bem-vindo(a) ao Cidade Alta Minigame!
+                        </p>
+
+                        <small className="text-slate-400 font-bold ">Clique no botão abaixo para iniciar 👇</small>
+                    </div>
+
+                    <button className="btn-primary w-4/5">
+                        Iniciar
+                    </button>
+                </div>
             </Card>
         </main>
     )
